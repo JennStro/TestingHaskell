3ac5c5b1740dd59acd63a23d1daa60207213005e
JennStro <jennys1994@gmail.com>
2021-03-04 14:37:53 +0100

    Refactoring

--- tester.hs
+++ tester.hs
@@ -4,6 +4,6 @@ module Tester where
 type Message = String 
 
 assertEquals :: (Eq a, Show a) => a -> a -> Message -> IO()
-assertEquals exp act message = writeFile "out.txt"  $ show (exp == act) ++ " " ++ "Message: " ++ message ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
+assertEquals exp act message = writeFile "out.txt"  $ show (exp == act) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
 
 
