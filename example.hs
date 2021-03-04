4cbdbed23acfbf7976fdb2425adfa7aa285fe627
JennStro <jennys1994@gmail.com>
2021-03-04 14:35:46 +0100

    print actual and expected

--- example.hs
+++ example.hs
@@ -3,4 +3,3 @@ import Tester
 test :: IO() 
 test = 
         assertEquals 1 1 "testmessage"
-       -- writeFile "out.txt"  $ show (1 == 1) ++ " " ++ "1 == 1 should pass" ++ "\n" ++ show (1 == 2) ++ " " ++ "1 == 2 should fail" ++ "\n"
--- tester.hs
+++ tester.hs
@@ -3,7 +3,7 @@ module Tester where
 
 type Message = String 
 
-assertEquals :: (Eq a, Eq a) => a -> a -> Message -> IO()
-assertEquals exp act message = writeFile "out.txt"  $ show (exp == act) ++ " " ++ message ++ "\n"
+assertEquals :: (Eq a, Show a) => a -> a -> Message -> IO()
+assertEquals exp act message = writeFile "out.txt"  $ show (exp == act) ++ " " ++ "Message: " ++ message ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
 
 
