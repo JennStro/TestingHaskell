e43429c3d162ce6bbba10500ec2da47322a07600
JennStro <jennys1994@gmail.com>
2021-03-05 09:21:21 +0100

    refactoring write to file

--- tester.hs
+++ tester.hs
@@ -4,7 +4,11 @@ module Tester where
 type Message = String 
 
 shouldBeEqual :: (Eq a, Show a) => a -> a -> Message -> IO()
-theseShouldBeEqual exp act message = appendFile "out.txt"  $ show (exp == act) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
+shouldBeEqual exp act message = writeToOutFile "out.txt" "shouldBeEqual" (exp == act) message exp act --appendFile "out.txt"  $ show (exp == act) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
 
 shouldNotBeEqual :: (Eq a, Show a) => a -> a -> Message -> IO()
-theseShouldNotBeEqual exp act message = appendFile "out.txt"  $ show (not(exp == act)) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
+shouldNotBeEqual exp act message = writeToOutFile "out.txt" "shouldNotBeEqual" (not(exp == act)) message exp act --appendFile "out.txt"  $ show (not(exp == act)) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
+
+
+writeToOutFile :: (Eq a, Show a) => String -> String -> Bool -> String -> a -> a -> IO()
+writeToOutFile filename testMethodname testPassed message expected actual = appendFile filename $ show testPassed ++ " " ++ "[ " ++ message ++ " ]" ++ " (Expected: " ++ show expected ++ " Actual: " ++ show actual ++ " in " ++ testMethodname ++ ") " ++ "\n"
