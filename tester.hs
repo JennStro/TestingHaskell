e77bbff15f50dfe8b951b5f80745be079af56ebf
JennStro <jennys1994@gmail.com>
2021-03-05 09:52:01 +0100

    refactoring

--- tester.hs
+++ tester.hs
@@ -1,13 +1,14 @@
 
-module Tester where 
+module Tester (shouldAlwaysBeTrue, shouldBeEqual, shouldNotBeEqual) where 
+
 
 type Message = String 
 
 shouldBeEqual :: (Eq a, Show a) => a -> a -> Message -> IO()
-shouldBeEqual exp act message = writeToOutFile "out.txt" "shouldBeEqual" (exp == act) message exp act --appendFile "out.txt"  $ show (exp == act) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
+shouldBeEqual exp act message = writeToOutFile "out.txt" "shouldBeEqual" (exp == act) message exp act 
 
 shouldNotBeEqual :: (Eq a, Show a) => a -> a -> Message -> IO()
-shouldNotBeEqual exp act message = writeToOutFile "out.txt" "shouldNotBeEqual" (not(exp == act)) message exp act --appendFile "out.txt"  $ show (not(exp == act)) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
+shouldNotBeEqual exp act message = writeToOutFile "out.txt" "shouldNotBeEqual" (not(exp == act)) message exp act 
 
 shouldAlwaysBeTrue :: Bool -> Message -> IO()
 shouldAlwaysBeTrue condition message = writeToOutFile "out.txt" "shouldAlwaysBeTrue" (condition == True) message True condition 
