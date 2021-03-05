34aa6d0fc94c5c50cc0d76551d98286775a5c6b3
JennStro <jennys1994@gmail.com>
2021-03-05 09:29:03 +0100

    Add shouldAlwaysBeTrue

--- example.hs
+++ example.hs
@@ -4,3 +4,5 @@ test :: IO()
 test = do
         shouldBeEqual 1 1 "1 == 1"
         shouldNotBeEqual 1 1 "1 /= 1"
+        shouldAlwaysBeTrue (1 == 2) "my test"
+        shouldAlwaysBeTrue (3 == 3) "my succeeding test"
--- tester.hs
+++ tester.hs
@@ -9,6 +9,8 @@ shouldBeEqual exp act message = writeToOutFile "out.txt" "shouldBeEqual" (exp ==
 shouldNotBeEqual :: (Eq a, Show a) => a -> a -> Message -> IO()
 shouldNotBeEqual exp act message = writeToOutFile "out.txt" "shouldNotBeEqual" (not(exp == act)) message exp act --appendFile "out.txt"  $ show (not(exp == act)) ++ " " ++ "[" ++ message ++ "]" ++ " (Expected: " ++ show exp ++ " Actual: " ++ show act ++ ") " ++ "\n"
 
+shouldAlwaysBeTrue :: Bool -> Message -> IO()
+shouldAlwaysBeTrue condition message = writeToOutFile "out.txt" "shouldAlwaysBeTrue" (condition == True) message True condition 
 
 writeToOutFile :: (Eq a, Show a) => String -> String -> Bool -> String -> a -> a -> IO()
 writeToOutFile filename testMethodname testPassed message expected actual = appendFile filename $ show testPassed ++ " " ++ "[ " ++ message ++ " ]" ++ " (Expected: " ++ show expected ++ " Actual: " ++ show actual ++ " in " ++ testMethodname ++ ") " ++ "\n"
